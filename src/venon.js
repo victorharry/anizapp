7a97669ec34e626167e841b0c76fa24d61bcbe59
Harry <victorhch@live.com>
2021-05-30 14:23:51 -0300

    venon config options

--- src/venon.js
+++ src/venon.js
@@ -2,7 +2,25 @@ const venom = require('venom-bot');
 const { dockStart } = require('@nlpjs/basic');
 
 venom
-    .create()
+    .create(
+        {
+            folderNameToken: 'tokens', //folder name when saving tokens
+            headless: true, // Headless chrome
+            devtools: false, // Open devtools by default
+            useChrome: true, // If false will use Chromium instance
+            debug: false, // Opens a debug session
+            logQR: true, // Logs QR automatically in terminal
+            browserWS: '', // If u want to use browserWSEndpoint
+            browserArgs: [''], //Original parameters  ---Parameters to be added into the chrome browser instance
+            puppeteerOptions: {
+                executablePath: '/usr/bin/chromium-browser',
+            },
+            disableSpins: false, // Will disable Spinnies animation, useful for containers (docker) for a better log
+            disableWelcome: false, // Will disable the welcoming message which appears in the beginning
+            updatesLog: true, // Logs info updates automatically in terminal
+            createPathFileToken: true, //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
+          },
+    )
     .then((client) => start(client))
     .catch((erro) => {
         console.log(erro);
