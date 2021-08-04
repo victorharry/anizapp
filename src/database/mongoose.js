08c824b363ba609d91db9e942bd3b5a9d72accf8
victorharry <victorhch@live.com>
2021-08-04 12:42:47 -0300

    mongoose auth conn

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 
 dotenv.config()
-mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb://${process.env.DB_USER ? `${process.env.DB_USER }:${process.env.DB_PASSWORD}` : null}@localhost:27017/${process.env.DB_NAME}?authSource=admin`);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
